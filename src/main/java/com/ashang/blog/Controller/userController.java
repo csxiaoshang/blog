620c7638f4425ce7171f09cc0e7a04e8b856da5a
csxiaoshang <970090853@qq.com>
2018-11-26 16:57:05 +0800

    增加用户注销功能，session中抹去用户属性

--- src/main/java/com/ashang/blog/Controller/userController.java
+++ src/main/java/com/ashang/blog/Controller/userController.java
@@ -1,13 +1,17 @@
 package com.ashang.blog.Controller;
 
 import com.ashang.blog.Entity.User;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import sun.plugin.liveconnect.SecurityContextHelper;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@Controller
+@RestController
 @RequestMapping(path = "/user")
 //@CrossOrigin(origins = "http://localhost:8001",maxAge = 3600)
 public class userController{
@@ -18,5 +22,16 @@ public class userController{
             User user= (User) httpSession.getAttribute("user");
             System.out.println(user);
             return user;
+        }
+
+        @GetMapping(path = "/logout")
+        public String logout(HttpServletRequest request, HttpServletResponse response){
+
+        HttpSession session=request.getSession(false);
+        if(session==null){
+            return "false";
+        }
+        session.removeAttribute("user");
+        return "success";
         }
 }
